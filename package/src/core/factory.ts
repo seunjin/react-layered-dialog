4343202f3a8032acb05becfb0092dd04f4c76318
seunjin <jinseun@gmail.com>
2025-08-19 00:54:19 +0900

    chore: useDialog에 대한 주석 추가

--- package/src/core/factory.ts
+++ package/src/core/factory.ts
@@ -9,6 +9,12 @@ export function createDialogManager<T extends BaseDialogState>(
 ) {
   const manager = new DialogManager<T>(config);
 
+  /**
+   * DialogManager의 상태를 구독하고 현재 열려있는 모든 다이얼로그 인스턴스를 반환하는 React Hook입니다.
+   * 이 훅은 컴포넌트가 다이얼로그 목록의 변경 사항을 자동으로 감지하고 다시 렌더링하도록 합니다.
+   *
+   * @returns {DialogInstance<T>[]} 현재 열려있는 모든 다이얼로그 인스턴스의 배열
+   */
   const useDialogs: () => DialogInstance<T>[] = () => {
     return useSyncExternalStore(manager.subscribe, manager.getSnapshot);
   };
