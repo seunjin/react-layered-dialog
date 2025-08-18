e98b528885d15f205c5786820128d049e4c0f18c
seunjin <jinseun@gmail.com>
2025-08-19 01:52:02 +0900

    chore: package export type 정의

--- package/src/index.ts
+++ package/src/index.ts
@@ -1,3 +1,4 @@
 // export * from './core/types';
 export { DialogManager } from './core/manager';
 export { createDialogManager } from './core/factory';
+export type { DialogState, DialogInstance } from './core/types';
