aab53e3180684b26948281c8937688991e5aa65f
seunjin <jinseun@gmail.com>
2025-09-11 13:01:49 +0900

    chore:  codeBlock import 수정

--- example/src/pages/guides/OpeningAndClosing.tsx
+++ example/src/pages/guides/OpeningAndClosing.tsx
@@ -1,7 +1,7 @@
-import { CodeBlock } from '@/components/docs/Code-Block';
 import { InlineCode } from '@/components/docs/InlineCode';
 import { DocArticle } from '@/components/docs/DocArticle';
 import { Section } from '@/components/docs/Section';
+import { CodeBlock } from '@/components/docs/CodeBlock';
 
 const openDialogCode = `import { openDialog } from '@/lib/dialogs';
 
@@ -37,9 +37,7 @@ closeDialog(dialogId);
 
 export const OpeningAndClosing = () => (
   <DocArticle title="Opening & Closing Dialogs">
-    <p className="lead">
-      다이얼로그를 열고 닫는 기본적인 방법을 알아봅니다.
-    </p>
+    <p className="lead">다이얼로그를 열고 닫는 기본적인 방법을 알아봅니다.</p>
 
     <Section as="h2" id="open-dialog" title="openDialog">
       <p>
@@ -53,16 +51,16 @@ export const OpeningAndClosing = () => (
         특히 <InlineCode>confirm</InlineCode>과 같이 사용자 상호작용의 결과가
         필요한 다이얼로그의 경우, <InlineCode>openDialog</InlineCode>는{' '}
         <InlineCode>Promise</InlineCode>를 반환합니다. 다이얼로그가 닫힐 때{' '}
-        <InlineCode>resolve</InlineCode> 함수에 값을 전달하여 결과를 반환받을
-        수 있습니다.
+        <InlineCode>resolve</InlineCode> 함수에 값을 전달하여 결과를 반환받을 수
+        있습니다.
       </p>
     </Section>
 
     <Section as="h2" id="close-dialog" title="closeDialog">
       <p>
-        <InlineCode>closeDialog</InlineCode> 함수는 다이얼로그를 닫습니다.
-        인자 없이 호출하면 현재 열려있는 다이얼로그 중 가장 위에 있는(z-index가
-        가장 높은) 다이얼로그를 닫습니다. 특정 다이얼로그를 닫고 싶다면,{' '}
+        <InlineCode>closeDialog</InlineCode> 함수는 다이얼로그를 닫습니다. 인자
+        없이 호출하면 현재 열려있는 다이얼로그 중 가장 위에 있는(z-index가 가장
+        높은) 다이얼로그를 닫습니다. 특정 다이얼로그를 닫고 싶다면,{' '}
         <InlineCode>openDialog</InlineCode>가 반환하는{' '}
         <InlineCode>id</InlineCode> 를 인자로 전달하면 됩니다.
       </p>
